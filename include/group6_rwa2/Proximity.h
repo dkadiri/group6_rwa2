50bcfe8ab684a89230e71e0eb5400e7a8b4ac259
dkadiri <dkadirim@terpmail.umd.edu>
2020-03-03 19:54:46 -0500

    create proximity class

---
+++ include/group6_rwa2/Proximity.h
@@ -0,0 +1,17 @@
+/*
+ * Proximity.h
+ *
+ *  Created on: Mar 3, 2020
+ *      Author: dinesh
+ */
+
+#ifndef GROUP6_RWA2_SRC_PROXIMITY_H_
+#define GROUP6_RWA2_SRC_PROXIMITY_H_
+
+class Proximity {
+public:
+	Proximity();
+	 ~Proximity();
+};
+
+#endif /* GROUP6_RWA2_SRC_PROXIMITY_H_ */
---
+++ src/Proximity.cpp
@@ -0,0 +1,18 @@
+/*
+ * Proximity.cpp
+ *
+ *  Created on: Mar 3, 2020
+ *      Author: dinesh
+ */
+
+#include "Proximity.h"
+
+Proximity::Proximity() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Proximity::~Proximity() {
+	// TODO Auto-generated destructor stub
+}
+
